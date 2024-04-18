7989ebc79ba4d8443d18ae47d5760f380e80bbf8
Naf <salaudeennafeesah97gmail.com@>
2024-04-18 15:49:25 -0700

    f2

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,7 +3,7 @@
 const request = require('request');
 const movie_id = process.argv[2];
 
-export default async function nameloader(movie_id) {
+default async function nameloader(movie_id) {
     let res = [];
 
     const film = request(`https://swapi-api.alx-tools.com/api/films/${movie_id}`, (error, response, body) => {

ce0ca1d7ee3b41a3979aa3958ddfa4a2aa3dc2f8
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-04-18 23:51:23 +0100

    still fixing3

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,21 +3,28 @@
 const request = require('request');
 const movie_id = process.argv[2];
 
-default async function nameloader(movie_id) {
-    let res = [];
+async function nameloader(movie_id) {
+    return new Promise((resolve, reject) => {
+        request(`https://swapi-api.alx-tools.com/api/films/${movie_id}`, (error, response, body) => {
+            if (error) {
+                reject(error);
+            }
+            if (response.statusCode !== 200) {
+                reject(`Request failed with status code ${response.statusCode}`);
+            }
 
-    const film = request(`https://swapi-api.alx-tools.com/api/films/${movie_id}`, (error, response, body) => {
-        if (error) {
-            return;
-        }
-        if (response.statusCode !== 200) {
-          return;
-        }
-        
-        return body;
+            resolve(body);
+        });
     });
 }
 
+async function main() {
+    try {
+        const result = await nameloader(movie_id);
+        console.log(result);
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
 
-const result = nameloader(movie_id);
-console.log(result);
+main();

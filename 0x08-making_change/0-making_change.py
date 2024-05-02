470904c8f5eef5a09d324ba4855d26492e021eae
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-05-02 23:45:57 +0100

    make change f5

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """making changes"""
 
-def changedynamic(coins, total, n, dp):
+def changedynamic(coins, total, dp):
     """To dynmically find the lowest combination"""
     dp[0] = 0
     if (total <= 0):
@@ -10,13 +10,12 @@ def changedynamic(coins, total, n, dp):
     for coin in coins:
         for i in range(coin, total + 1):
             dp[i] = min(dp[i], 1 + dp[i - coin])
-    return dp[total] if dp[total] != -12 else -1
+    return dp[total] if dp[total] != float('inf') else -1
 
 def makeChange(coins, total):
     """
     determine the fewest number of coins needed to meet
     a given amount total
     """
-    n = len(coins)
-    dp = [-12 for i in range(total + 1)]
-    return changedynamic(coins, total, n, dp)
+    dp = [float('inf')] * (total + 1)
+    return changedynamic(coins, total, dp)

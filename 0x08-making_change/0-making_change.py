91bef27f511511a5872866c5f19051e6fe7040c9
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-05-02 13:16:34 +0100

    make change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""making changes"""
+
+def changedynamic(coins, total, n, dp):
+    """To dynmically find the lowest combination"""
+    if (total <= 0):
+        return 0
+    if (n == 0):
+        return -1 
+    
+    if (dp[n][total] != -1):
+        return dp[n][total]
+    
+    dp[n][total] = (changedynamic(coins, total - (coins[n - 1]), n, dp)
+                    + changedynamic(coins, total, n -1, dp))
+    return dp[n][total]
+
+def makeChange(coins, total):
+    """
+    determine the fewest number of coins needed to meet
+    a given amount total
+    """
+    n = len(coins)
+    dp = [[-1 for i in range(total + 1)] for j in range(n + 1)]
+    return changedynamic(coins, total, n, dp)
---
+++ 0x08-making_change/README.md
@@ -0,0 +1,2 @@
+-- Project Objective
+It tackle a classic problem from the domain of dynamic programming and greedy algorithms: the coin change problem. The objective is to find the minimum number of coins required to make up a given total amount, given a list of coin denominations.

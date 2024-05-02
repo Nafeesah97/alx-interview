6b4a99e484d17b60c26f704a986a63e8986d845c
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-05-02 22:09:29 +0100

    make change f4

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,15 +3,19 @@
 
 def changedynamic(coins, total, n, dp):
     """To dynmically find the lowest combination"""
-    if (total <= 0 or n == 0):
+    if (total <= 0):
         return 0
     
+    if n == 0:
+        return float('inf')
+
     if (dp[n][total] != -1):
         return dp[n][total]
     
-    if (total in coins):
-        return -1
-    dp[n][total] = min(changedynamic(coins, total - (coins[n - 1]), n, dp),
+    if total < coins[n - 1]:
+        dp[n][total] = changedynamic(coins, total, n - 1, dp)
+    else:
+        dp[n][total] = min(1 + changedynamic(coins, total - (coins[n - 1]), n, dp),
                        changedynamic(coins, total, n -1, dp))
     return dp[n][total]
 

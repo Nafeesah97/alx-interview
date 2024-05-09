986d7c9766634e025047a4f474ccf9968ed9bb57
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-05-09 22:21:33 +0100

    island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+island perimeter module
+Author: Nafeesah
+"""
+
+
+def island_perimeter(grid):
+    """returns the perimeter of the island described in grid"""
+    total = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if i != 0:
+                if grid[i-1][j] == 0:
+                    total += 1
+            if j != 0:
+                if grid[i][j - 1] == 0:
+                    total += 1
+            if i != (len(grid) - 1):
+                if grid[i + 1][j] == 0:
+                    total += 1
+            if j != (len(grid[i]) - 1):
+                if grid[i][j + 1] == 0:
+                    total += 1
+    return total
